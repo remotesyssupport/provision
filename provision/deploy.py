8c12b38c84defba5ac71f5a1b6803f04ac01aa15
Jay Doane <jay.s.doane@gmail.com>
2011-04-18 13:08:44 -0700

    Refactor deployment code for use as library

--- provision/deploy.py
+++ provision/deploy.py
@@ -11,8 +11,6 @@ def parser():
     parser = argparse.ArgumentParser()
     config.add_auth_args(parser, config)
 
-    # parser.add_argument('subvars', nargs='*',
-    #                     help='key=value pairs of script substitution variables')
     parser.add_argument('-b', '--bundles', default=[], action='append')
     parser.add_argument('-d', '--description-file')
     parser.add_argument('-i', '--image', default=config.DEFAULT_IMAGE_NAME)
@@ -26,17 +24,21 @@ def parser():
     parser.add_argument('-x', '--prefix', default=config.DEFAULT_NAME_PREFIX)
     return parser
 
+def deploy(args=None):
+    parsed = config.reconfig(parser, args)
+    deployment = nodelib.Deployment(name=parsed.name, bundles=parsed.bundles,
+                                    prefix=parsed.prefix, subvars=parsed.subvars)
+    driver = nodelib.get_driver(parsed.secret_key, parsed.userid, parsed.provider)
+    node = deployment.deploy(driver, parsed.location, parsed.size, parsed.image)
+    if parsed.verbose:
+        print(node)
+    if parsed.description_file:
+        node.write_json(parsed.description_file)
+    return node
+
 def main():
     try:
-        parsed = config.reconfig(parser)
-        deployment = nodelib.Deployment(name=parsed.name, bundles=parsed.bundles,
-                                        prefix=parsed.prefix, subvars=parsed.subvars)
-        driver = nodelib.get_driver(parsed.secret_key, parsed.userid, parsed.provider)
-        node = deployment.deploy(driver, parsed.location, parsed.size, parsed.image)
-        if parsed.verbose:
-            print(node)
-        if parsed.description_file:
-            node.write_json(parsed.description_file)
+        node = deploy()
         return sum([sd.exit_status for sd in node.script_deployments])
     except:
         traceback.print_exc(file=sys.stderr)
