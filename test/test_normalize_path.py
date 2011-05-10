6ed4a34b286c8380997c2bd3cf7dfda4c49f4d1e
Jay Doane <jay.s.doane@gmail.com>
2011-05-10 11:02:25 -0700

    Add test for trailing slash bug

--- test/test_normalize_path.py
+++ test/test_normalize_path.py
@@ -11,5 +11,8 @@ class Test(unittest.TestCase):
     def test_normalize_path_relative(self):
         assert config.normalize_path('foo/bar', '/baz') == '/baz/foo/bar'
 
-    def test_normalize_path_user(self):
+    def test_normalize_path_userexpand(self):
         assert config.normalize_path('~', '/baz') == os.path.expanduser('~')
+
+    def test_normalize_path_trailing_slash(self):
+        assert config.normalize_path('/foo/') == '/foo'
