b0e3b1a3a3bfc13973e9d81ec4b83ac483ea82eb
Jay Doane <jay.s.doane@gmail.com>
2011-08-06 19:07:50 -0700

    Much better to create separate file than to modify /etc/apt/sources.list

--- provision/defaults/scripts/zenoss.sh
+++ provision/defaults/scripts/zenoss.sh
@@ -1,4 +1,4 @@
-echo "deb http://dev.zenoss.org/deb main stable" >> /etc/apt/sources.list
+echo "deb http://dev.zenoss.org/deb main stable" > /etc/apt/sources.list.d/zenoss.list
 apt-get update
 apt-get -y --force-yes install zenoss-stack
 /etc/init.d/zenoss-stack start
