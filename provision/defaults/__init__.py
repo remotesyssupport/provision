fdbaa4a8299bc007a9fb14c824ccd9179c36b8af
Jay Doane <jay.s.doane@gmail.com>
2011-10-03 18:23:46 -0700

    Add basic tmux configuration

--- provision/defaults/__init__.py
+++ provision/defaults/__init__.py
@@ -7,7 +7,7 @@ def init(config):
 
     config.add_bundle('bootstrap-python', ['bootstrap-python.sh'])
     config.add_bundle('dev', ['emacs.sh', 'screen.sh'],
-                      ['/root/.emacs.d/init.el', '/root/.screenrc'])
+                      ['/root/.emacs.d/init.el', '/root/.screenrc', '/root/.tmux.conf'])
     config.add_bundle('hudson', ['jre.sh', 'postfix.sh', 'hudson.sh'])
     config.add_bundle('libcloud', ['libcloud-env.sh'])
     config.add_bundle('mta', ['postfix.sh'])
